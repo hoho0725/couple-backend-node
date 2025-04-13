6ebaaa77b49764951beee9c22c74c4cfb9838bcd
hoho0725 <chs3112a@gmail.com>
2025-04-13 06:13:17 +0000

    Initial commit: couple-backend-node code

--- routes/diaryRoutes.js
+++ routes/diaryRoutes.js
@@ -1,9 +1,8 @@
-// routes/diaryRoutes.js
 const express = require('express');
 const router = express.Router();
 const Diary = require('../models/Diary'); // Diary 모델 불러오기
 
-// [POST] 새로운 일기 작성 (기존 코드)
+// [POST] 새로운 일기 작성
 router.post('/', async (req, res) => {
   const { type, content } = req.body;
   try {
@@ -15,36 +14,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-// [GET] 일기 조회 API : 작성된 일기를 모두 조회함 (최신 순으로 정렬)
-router.get('/', async (req, res) => {
-  try {
-    // Diary 컬렉션에서 모든 일기를 조회하고, createdAt(작성일) 내림차순 정렬
-    const diaries = await Diary.find().sort({ createdAt: -1 });
-    res.json({ diaries });
-  } catch (error) {
-    res.status(500).json({ message: '서버 오류', error });
-  }
-});
-
-module.exports = router;
-// routes/diaryRoutes.js
-const express = require('express');
-const router = express.Router();
-const Diary = require('../models/Diary');
-
-// [POST] 새로운 일기 작성 (기존 코드)
-router.post('/', async (req, res) => {
-  const { type, content } = req.body;
-  try {
-    const newDiary = new Diary({ type, content });
-    await newDiary.save();
-    res.status(201).json({ message: '일기 작성 완료!', diary: newDiary });
-  } catch (error) {
-    res.status(500).json({ message: '서버 오류', error });
-  }
-});
-
-// [GET] 일기 조회 (모든 일기를 최신순으로)
+// [GET] 일기 조회 (모든 일기를 최신순으로 정렬)
 router.get('/', async (req, res) => {
   try {
     const diaries = await Diary.find().sort({ createdAt: -1 });
@@ -54,7 +24,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-// [PUT] 일기 수정 : URL 예) /diaries/일기ID
+// [PUT] 일기 수정 : URL 예) /diaries/:id
 router.put('/:id', async (req, res) => {
   const diaryId = req.params.id;
   const { type, content } = req.body;
@@ -73,7 +43,7 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-// [DELETE] 일기 삭제 : URL 예) /diaries/일기ID
+// [DELETE] 일기 삭제 : URL 예) /diaries/:id
 router.delete('/:id', async (req, res) => {
   const diaryId = req.params.id;
   try {
