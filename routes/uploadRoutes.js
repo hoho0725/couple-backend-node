2f4a0cfe96bd6a36ea794fd14a1ea265af2c1de8
최호호 <chs3112a@gmail.com>
2025-04-20 06:12:37 +0000

    파일 리스트 다운로드

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 // 파일 시스템 설정 (모든 파일 업로드 가능)
 // 운영 시에는 AWS S3와 같은 외부 스토리지를 사용하는 것이 좋습니다.
@@ -25,6 +26,28 @@ router.post('/', upload.single('file'), (req, res) => {
     return res.status(400).json({ message: '파일 업로드 실패' });
   }
   res.status(200).json({ message: '파일 업로드 성공!', file: req.file });
+});
+
+// 파일 목록 반환 API
+router.get('/files', (req, res) => {
+  const directoryPath = path.join(__dirname, '../uploads');
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      return res.status(500).json({ message: '파일 목록을 가져오는 데 실패했습니다.' });
+    }
+    res.status(200).json({ files });
+  });
+});
+
+// 파일 다운로드 API
+router.get('/download/:filename', (req, res) => {
+  const fileName = req.params.filename;
+  const filePath = path.join(__dirname, '../uploads', fileName);
+  res.download(filePath, fileName, (err) => {
+    if (err) {
+      return res.status(500).json({ message: '파일 다운로드 실패' });
+    }
+  });
 });
 
 module.exports = router;
